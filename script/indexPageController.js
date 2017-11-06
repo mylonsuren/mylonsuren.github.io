ce014686a1e57e14a31b2858893f165874523f55
Mylon <mylonsuren@yahoo.ca>
2017-11-06 17:24:14 -0500

    Updated site, added icons and suck

--- script/indexPageController.js
+++ script/indexPageController.js
@@ -3,14 +3,39 @@ var app = angular.module("app", []);
 
 app.controller('indexPageController', function($scope) {
 
-  $scope.listofProjects = {
-    "projectOne": {
-      name: 'Stock Market Simulator',
-      dateCompleted: new Date(),
-      shortDescription: 'This was a project.',
-      longDescription: 'This was a long project.',
-      toolsUsed: ['Javascript', 'HTML', 'CSS', 'Angular']
+    // List of Projects
+    $scope.listofProjects = {
+        "projectOne": {
+            name: 'Stock Market Simulator',
+            dateCompleted: new Date(),
+            shortDescription: 'This was a project.',
+            longDescription: 'This was a long project.',
+            toolsUsed: ['Javascript', 'HTML', 'CSS', 'Angular']
+        }
     }
-  }
+
+    // Menu Click Variable
+    $scope.homeButton = 'active';
+    $scope.linksButton = '';
+    $scope.resumeButton = '';
+
+    $scope.menuClick = function(button) {
+        if (button === 'homeButton') {
+            $scope.linksButton = '';
+            $scope.resumeButton = '';
+            $scope.homeButton = 'active';
+        } else if (button === 'linksButton') {
+            $scope.linksButton = 'active';
+            $scope.resumeButton = '';
+            $scope.homeButton = '';
+        } else {
+            $scope.resumeButton = 'active';
+            $scope.linksButton = '';
+            $scope.homeButton = '';
+        }
+    }
+
+
+    $scope.name = 'Mylon Suren';
 
 });
